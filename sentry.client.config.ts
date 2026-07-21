24a53b15dd76a44338117c39824e024f5d93398f
Lucas Brunini <lucasfelipebrunini@gmail.com>
2026-07-20 23:36:21 -0300

    fix(sentry): ignore network disconnection errors Failed to fetch and Load failed (Fixes JAVASCRIPT-NEXTJS-8 and JAVASCRIPT-NEXTJS-E)

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -10,7 +10,10 @@ Sentry.init({
     /NotFoundError/i,
     /The object can not be found here/i,
     /Failed to execute 'removeChild'/i,
+    /Failed to execute 'insertBefore'/i,
     /The node to be removed is not a child of this node/i,
+    /Failed to fetch/i,
+    /Load failed/i,
   ],
   beforeSend(event) {
     if (event.exception?.values?.some(v => v.type === 'RangeError' && v.value?.includes('stack size'))) {
